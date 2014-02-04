81ea8dae4a346df638bb510a2ccd9bed1378107d
Lonewolf <nikhilkalige@gmail.com>
2014-02-04 23:59:03 +0530

    Removed redudant functions Made changes based on comments by smartass using override_save_as instead of save_as

--- multiple_files/Readme.md
+++ multiple_files/Readme.md
@@ -12,10 +12,10 @@ Settings
     MULTIPLE_FILES_RENDER_META: 'render_multiple'
 
     # This sets the name of list offered in the template
-    MULTIPLE_FILES_OUTPUT_LIST: 'content_list'
+    MULTIPLE_FILES_OUTPUT_LIST: 'article_list'
 
-For the plugin to work, **'RENDER_MULTIPLE'** metadata should be set in the article. Besides that the files that need to be rendered as as single article should have same value set for **'save_as'** metadata in the articles
+For the plugin to work, **'render_multiple'** metadata should be set in the article. Besides that the files that need to be rendered as as single article should have same value set for **'save_as'** metadata in the articles
 
-Content list is available in the template as a dictionary by the name **'content_list'**, with key set as title of the article.
+Content list is available in the template as a dictionary by the name **'article_list'**, with key set as title of the article.
 
 
--- multiple_files/multiple.py
+++ multiple_files/multiple.py
@@ -2,12 +2,6 @@ from pelican import signals
 from pelican.utils import slugify
 
 
-def initialize(pelican):
-    if pelican:
-        pelican.settings.setdefault('MULTIPLE_FILES_RENDER_META', 'render_multiple')
-        pelican.settings.setdefault('MULTIPLE_FILES_OUTPUT_LIST', 'content_list')
-
-
 def multiple_articles(generator):
     multiple_files(generator, 'articles')
 
@@ -17,21 +11,15 @@ def multiple_pages(generator):
 
 
 def multiple_files(generator, file_class):
-    file_marker = generator.settings['MULTIPLE_FILES_RENDER_META']
-    list_name = generator.settings['MULTIPLE_FILES_OUTPUT_LIST']
+    file_marker = generator.settings.get('MULTIPLE_FILES_RENDER_META', 'render_multiple')
+    list_name = generator.settings.get('MULTIPLE_FILES_OUTPUT_LIST', 'article_list')
+
     # list of files which have the marker
-    file_multi = []
-    # index of files in generator
-    file_mulit_index = []
-    # extract files which have the marker set and get their index in the list
-    for idx, article in enumerate(getattr(generator, file_class)):
-        if hasattr(article, file_marker):
-            file_multi.append(article)
-            file_mulit_index.append(idx)
+    file_multi = [article for article in getattr(generator, file_class) if hasattr(article, file_marker)]
 
     # delete files from generator
-    for index in reversed(file_mulit_index):
-        del(getattr(generator, file_class)[index])
+    for article in file_multi:
+        getattr(generator, file_class).remove(article)
 
     # get the save_as metadata
     save_url_list = extract_save_url(file_multi)
@@ -42,31 +30,30 @@ def multiple_files(generator, file_class):
         link_index = 0
         index = 1
         for article in file_multi:
-            if article.save_as == link:
-                temp_content = article.content
+            if article.override_save_as == link:
                 temp_title = slugify(article.title)
                 if index == 1:
                     file_list.append(article)
                     setattr(file_list[link_index], list_name, dict())
                     index += 1
-                getattr(file_list[link_index], list_name)[temp_title] = temp_content
-
+                getattr(file_list[link_index], list_name)[temp_title] = article
         link_index += 1
 
     # append the generated list to generator object
     for article in file_list:
         getattr(generator, file_class).append(article)
+    print("")
 
 
 def extract_save_url(file_list):
     save_link_list = []
     for article in file_list:
-            if article.save_as not in save_link_list:
-                save_link_list.append(article.save_as)
+            if article.override_save_as not in save_link_list:
+                save_link_list.append(article.override_save_as)
+    #save_link_list = [article.override_save_as for article in file_list if article.override_save_as not in save_link_list]
     return save_link_list
 
 
 def register():
-    signals.initialized.connect(initialize)
     signals.article_generator_finalized.connect(multiple_articles)
     signals.page_generator_finalized.connect(multiple_pages)
