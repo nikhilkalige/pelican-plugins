baa140ff8e7ede62e598e94ded7dbc8b943bc493
Lonewolf <nikhilkalige@gmail.com>
2014-02-24 15:43:30 +0530

    Detection for valid output path

--- incremental_update/incremental.py
+++ incremental_update/incremental.py
@@ -1,6 +1,5 @@
 import git
 from pelican import signals
-from pelican.utils import slugify
 import logging
 import os
 import sys
@@ -14,57 +13,70 @@ _incremental_update = True
 def initialize(pelican):
     global _incremental_update
     logger.debug("IUP: Begin")
-    # check if git is initialized
-    try:
-        repo = git.Repo("")
-    except git.InvalidGitRepositoryError:
-        # repo is not initialized, initialize now
-        repo = git.Repo.init(".")
-        logger.debug("IUP: Initialize new repository")
-    create_gitignore()
-    if not commits_present(repo):
-        # there are no commits, quit now
-        logger.critical("IUP: There are no commits, please add commit changes and run again")
-        logger.critical("IUP: Exiting now")
-        sys.exit()
-    # get the current commit
-    current_commit = repo.commit()
-    # get the last commit based on which the content was generated
-    # temporarily we will use hardcoded data
-    last_commit = "18269fb124c1f9e96ec1f6246ee184f2e195886c"
-    last_commit = "f9d8519c10591a7aeff588b95ae42ee3b7ba9ab3"
-    # office pc
-    last_commit = "09e8fe4853862394a13be5a82e9944343e5cd93b"  # 3rd commit
-    last_commit = "f42e9e4a0cefb6b7c2e6c4a4a7bfad93cd435adc"  # 2nd commit
-    last_commit = "6d39c31f03b8995ade57fe9d9d5474ce6d3f5d84"  # 1st commit
-    # get the diff between the commits
-    diff = current_commit.diff(last_commit)
-    # check if there are any changes in config files
-    if diff:
-        for x in diff:
-            if x.a_blob.name in ["pelicanconf.py", "publishconf.py"]:
-                # config files have changed, regenerate content
-                _incremental_update = False
-                break
-    else:
-        # there are no changes between commits, time to exit pelican
-        logger.debug("IUP: There are no changes, exiting pelican")
+
+    # check whether output folder has generated content
+    # get output folder from settings
+    ouput_location = pelican.settings.get("OUTPUT_PATH")
+    if not os.path.isdir(ouput_location) or os.listdir(ouput_location) == []:
+        _incremental_update = False
+
     if _incremental_update is False:
-        logger.debug("IUP: Config files have changed, regenerate whole content")
+        logger.debug("IUP: Output path does not exist, IUP plugin disabled")
+
+    if _incremental_update is True:
+        # check if git is initialized
+        try:
+            repo = git.Repo("")
+        except git.InvalidGitRepositoryError:
+            # repo is not initialized, initialize now
+            repo = git.Repo.init(".")
+            logger.debug("IUP: Initialize new repository")
+        create_gitignore()
+        if not commits_present(repo):
+            # there are no commits, quit now
+            logger.critical("IUP: There are no commits, please add commit changes and run again")
+            logger.critical("IUP: Exiting now")
+            sys.exit()
+        # get the current commit
+        current_commit = repo.commit()
+        # get the last commit based on which the content was generated
+        # temporarily we will use hardcoded data
+        # home pc
+        last_commit = "18269fb124c1f9e96ec1f6246ee184f2e195886c"
+        last_commit = "f9d8519c10591a7aeff588b95ae42ee3b7ba9ab3"
+        # office pc
+        last_commit = "09e8fe4853862394a13be5a82e9944343e5cd93b"  # 3rd commit
+        last_commit = "6d39c31f03b8995ade57fe9d9d5474ce6d3f5d84"  # 1st commit
+        last_commit = "f42e9e4a0cefb6b7c2e6c4a4a7bfad93cd435adc"  # 2nd commit
+        # get the diff between the commits
+        diff = current_commit.diff(last_commit)
+        # check if there are any changes in config files
+        if diff:
+            for x in diff:
+                if x.a_blob.name in ["pelicanconf.py", "publishconf.py"]:
+                    # config files have changed, regenerate content
+                    _incremental_update = False
+                    break
+        else:
+            # there are no changes between commits, time to exit pelican
+            logger.debug("IUP: There are no changes, exiting pelican")
+
+        if _incremental_update is False:
+            logger.debug("IUP: Config files have changed, regenerate whole content")
 
 
 def aritcles_update(generator):
     global _incremental_update
     # skip if config has been changed or if output dir has been cleaned
     if _incremental_update is True:
-        logger.debug("IUP: Checking articles for changes")
+        logger.debug("IUP: Begin working on articles")
 
 
 def pages_update(generator):
     global _incremental_update
     # skip if config has been changed or if output dir has been cleaned
     if _incremental_update is True:
-        logger.debug("IUP: Checking pages for changes")
+        logger.debug("IUP: Begin working on pages")
 
 
 def create_gitignore():
