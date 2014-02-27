95afca405471b21fde39ac0eeb01b95c30022d5b
Lonewolf <nikhilkalige@gmail.com>
2014-02-27 23:34:02 +0530

    Minor error fixes Check if current commit is tagged

--- git-versioned-output/git_versioned_output.py
+++ git-versioned-output/git_versioned_output.py
@@ -17,7 +17,6 @@ def initialize(pelican):
     logger.debug("Begin")
     #get current working directory
     output_path_name = pelican.settings.get("GIT_VERSIONED_OUTPUT_TAG", "v-{tag}")
-    output_location_name = pelican.settings.get("GIT_VERSIONED_OUTPUT_LOCATION", "version")
     (working_path, content) = os.path.split(pelican.path)
     # check if git is initialized
     try:
@@ -31,6 +30,7 @@ def initialize(pelican):
     # if working directory is dirty ... quit plugin
     if not repo.is_dirty() and not repo.untracked_files:
         tags_list = repo.tags
+        current_commit = repo.commit()
         if not tags_list:
             # there are no tags, quit now
             logger.warning("There are no tags, output will generated as usual")
@@ -38,15 +38,18 @@ def initialize(pelican):
         if output_path_name.find("{tag}") < 1:
             logger.warning("Invalid plugin setting, output will be generated as usual")
             return
+        if tags_list[-1].commit != current_commit:
+            logger.warning("Current commit is not tagged, output will generated as usual")
+            return
 
         output_path_name = output_path_name.replace("{tag}", tags_list[-1].name)
-        output_path = os.path.join(working_path, output_location_name, output_path_name)
+        output_path = os.path.join(pelican.output_path, output_path_name)
         if os.path.isdir(output_path) and os.listdir(output_path):
             logger.warning("Output for tag %s has already been generated, quiting pelican", tags_list[-1].name)
             sys.exit(0)
         pelican.output_path = output_path
         pelican.settings["OUTPUT_PATH"] = output_path
-        logger.debug("Output will generated at asdfasdfasdf%s", output_path)
+        logger.debug("Output will generated at %s", output_path)
     else:
         logger.warning("Repo is dirty or there are untracked files, output will be generated as usual")
 
